6e7f3152bad3b1560a20ed980c9b5ac280f1c63a
Megan Da Costa <mdacos2@uwo.ca>
2026-06-06 11:48:39 -0400

    new groupings

---
+++ javaInterviewPrep/GroupData.java
@@ -0,0 +1,5 @@
+package javaInterviewPrep;
+
+public class GroupData {
+    
+}
---
+++ javaInterviewPrep/SortData.java
@@ -0,0 +1,7 @@
+package javaInterviewPrep;
+
+//Given a field ex. employees, sort by a value ex.salary, and return the top 5 highest paid employees
+
+public class SortData {
+    
+}
