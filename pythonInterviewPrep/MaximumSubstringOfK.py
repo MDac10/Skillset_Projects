76db239b7b278b0aed2c86298b3c51d230069ea9
Megan Da Costa <mdacos2@uwo.ca>
2026-07-02 10:51:25 -0400

    pure code practice

---
+++ pythonInterviewPrep/MaximumSubstringOfK.py
@@ -0,0 +1,6 @@
+# Interview Question: Maximum Substring of K
+# Given a string, of lowercase letters, and an integer k, find the largest substring of the string where the absolute ASCII value difference bwetween each pair of 
+# consecutive characters is less than or equal to k. return the length of the largest substring.
+
+
+
